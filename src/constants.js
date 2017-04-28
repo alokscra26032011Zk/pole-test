aa9c3603d0972a40517e2bb24e972cf27a92e36f
alokscra26032011Zk <alokscra26032011@gmail.com>
2017-04-28 20:58:31 +0530

    rm commit

--- src/constants.js
+++ src/constants.js
@@ -1,14 +1,3 @@
 const menuItems = [
-  {
-    title: 'The Next Web',
-    url: '/the-next-web',
-  },
-  {
-    title: 'TechCrunch',
-    url: '/techcrunch',
-  },
-  {
-    title: 'Ars Technica',
-    url: '/ars-technica',
-  },
+  
 ];
